057a39cb74a764b26214e18431642639e40d9143
Pavel <lovecars527@gmail.com>
2022-10-30 04:59:02 +0300

    initial

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -1,6 +1,5 @@
 package koschei.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,7 +7,6 @@ public class Island2 {
 
     private final Wood3 wood;
 
-    @Autowired
     public Island2(Wood3 wood) {
         this.wood = wood;
     }
